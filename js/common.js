f31886eb4efcb90a2cdb3ba717a10808eff1986f
yml523 <1329372025yml523>
2018-01-04 21:30:21 +0800

    图片处理

--- js/common.js
+++ js/common.js
@@ -1,34 +1,35 @@
+
 // 导航相关事件
 function menuFunc() {
   var Oparant = $('#header');
   var Omenu = $('#menu_box');
-  Oparant.find('.menu').click(function () {
-	  var ismenu = $(this).hasClass('menu_img');
-	  $(this).addClass('hide');
-	  $(this).siblings('img').removeClass('hide');
-	  if (ismenu) {	//判断点击当前对象是否显示导航
-	    $('body').css('overflow', 'hidden');
-	    Omenu.removeClass('hide');
-	  } else {
-	    $('body').css('overflow', 'auto');
-	    Omenu.addClass('hide');
-	  }
+  Oparant.find('.menu').click(function() {
+    var ismenu = $(this).hasClass('menu_img');
+    $(this).addClass('hide');
+    $(this).siblings('img').removeClass('hide');
+    if(ismenu) {  //判断点击当前对象是否显示导航
+      $('body').css('overflow','hidden');
+      Omenu.removeClass('hide');
+    }else {
+      $('body').css('overflow','auto');
+      Omenu.addClass('hide');          
+    }
   });
-}
+} 
 
 // 点击 我要合作
 function cooperation() {
   $('.menu_btn').click(function() {
     $('.menu_img').removeClass('hide').siblings('img').addClass('hide');
     $('body').css('overflow', 'auto');
-    $('#menu_box').addClass('hide');
+      Omenu.addClass('hide');
   });
 }
 
 
 //判断是否为空
 function isNull(str) {
-  if (!str || str === '' || str === null || str.length === 0) {
+  if(!str || str === "" || str === null || str.length === 0) {
     return true;
   }
   return false;
@@ -37,19 +38,20 @@ function isNull(str) {
 
 // tab切换事件
 function changeTab(obj) {
-  obj.click(function () {
+  obj.click(function() {
     var hasCas = $(this).hasClass('active');
 
-    if (!hasCas) {
+    if(!hasCas) {
       $(this).addClass('active');
       $(this).siblings().removeClass('active');
     }
-  });
+  })
 }
 
 
 // 返回顶部事件
 function backTop() {
+
   scrollTo(0,0);
 }
 
@@ -57,13 +59,13 @@ function backTop() {
 // 监听滚动条距顶部高度
 window.onscroll = function() {
   var topVal = document.documentElement.scrollTop || document.body.scrollTop;
+
   if(topVal > 200) {
     $('.backTop').removeClass('hide');
   }else {
     $('.backTop').addClass('hide');
   }
 }
-
 
 //查看大图
 function seeBigPic(obj) {
